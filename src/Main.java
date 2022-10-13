f639ff85f61d137f61ef06c797a90984dca198af
Anton <fynjycedjhjd@gmail.com>
2022-10-13 20:17:05 +0300

    сделал 3 заданиe

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,19 @@ public class Main {
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int sum = 0;
-        int maxCashe = 5;
+        int minCashe = 5;
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-            sum = sum + arr[i];
-            //задание 2
-            if (arr[i] > maxCashe) {
-                maxCashe = arr[i];
+            //задание 3
+            if (arr[i] < arr[i] + 1) {
+                minCashe = arr[i];
             }
         }
-        System.out.println(maxCashe);
-        System.out.println("Сумма затрат за месяц составила " + sum + " рублей");
+        System.out.println(minCashe);
 
         return arr;
     }
-
 }
 
 
