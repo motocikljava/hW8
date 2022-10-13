756f8e0274565559fb830e9b5771e156a405df55
Anton <fynjycedjhjd@gmail.com>
2022-10-13 20:55:44 +0300

    сделал 3 заданиe

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
+import java.util.Arrays;
+//задание 4
 public class Main {
     public static void main(String[] args) {
-        // задание 1
-        int[] arr = generateRandomArray();
-    }
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int minCashe = 5;
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-            //задание 3
-            if (arr[i] < arr[i] + 1) {
-                minCashe = arr[i];
-            }
-        }
-        System.out.println(minCashe);
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
 
-        return arr;
+        }
     }
 }
 
